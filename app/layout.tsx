4f90236f29e32f69ce1cbdbea8ebc11575e8f565
fgagliano <fabiogagliano@terra.com.br>
2026-01-01 21:12:51 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Notas Bernoulli",
+  description: "Controle de notas escolares"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
