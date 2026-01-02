3da5dc1c5746fdd0f43f345a4c8d1e3d86272d04
fgagliano <fabiogagliano@terra.com.br>
2026-01-01 22:05:45 -0300

    Update route.ts

--- app/api/alunos/[id]/route.ts
+++ app/api/alunos/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabaseServer } from "../../../../../lib/supabaseServer";
+import { supabaseServer } from "../../../../lib/supabaseServer";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
   const id = Number(params.id);
